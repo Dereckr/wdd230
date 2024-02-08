fe55224548f11f8aaf88055e4ea90f48f2ed9260
Dereckr <dereckromos@gmail.com>
2024-02-07 20:44:25 -0400

    update

--- lesson05/scripts/list.js
+++ lesson05/scripts/list.js
@@ -5,17 +5,19 @@ const list = document .querySelector("#list");
 button.addEventListener("click", ()=>{
     if (input.value != ""){
     const myChap = input.value;
-    input.value = "";
+    
 
     const li = document.createElement("li");
     const deleteButton = document.createElement("button");
     li.textContent = myChap;
     deleteButton.textContent = "❌";
+    deleteButton.setAttribute("aria-label","delete button")
     li.append(deleteButton);
     list.append(li);
     deleteButton.addEventListener("click", ()=>{
         li.remove();
         input.focus();
+        input.value = "";
     })
 
 
