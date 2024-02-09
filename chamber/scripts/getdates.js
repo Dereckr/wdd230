4cb486785cdfe511673785a65b7a10cde113169c
Dereckr <dereckromos@gmail.com>
2024-02-08 20:41:57 -0400

    update

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,10 +1,11 @@
+// Year
 
 const options = {year:"numeric"};
 
 document.getElementById("currentyear").textContent = new Date().toLocaleDateString("en-US", options);
 
 
-
+// Last Modified
 const optionsModified = {day:"numeric", month:"numeric", year:"numeric", hour:"numeric", minute:"numeric", second:"numeric"};
 
 document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US", optionsModified);
@@ -16,3 +17,26 @@ hamButton.addEventListener("click", ()=>{
     pages.classList.toggle("open");
     hamButton.classList.toggle("open");
 });
+
+// Dark Mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const borders = document.querySelectorAll(".borders");
+
+modeButton.addEventListener("click",() => {
+    if (modeButton.textContent.includes("🌛")){
+    main.style.backgroundColor = "#000";
+    main.style.color = "#fff";
+    borders.forEach(borders =>{ borders.style.borderColor = "#fff";
+});
+    modeButton.textContent="☀";
+} else{
+    main.style.backgroundColor = "#fff";
+    main.style.color = "#000";
+    borders.forEach(borders =>{ borders.style.borderColor = "#000";
+});
+    modeButton.textContent="🌛";
+
+}
+
+})
