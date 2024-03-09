19536d8bf7e4f7db1c9be525ee8f3324fceabfb3
Dereckr <dereckromos@gmail.com>
2024-03-08 21:10:43 -0400

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,8 +2,21 @@ const url = "data/members.json";
 
 const article = document.querySelector("article");
 
-async function getMembersData(url){
-    const response = await fetch(url);
-    const data = await response.json();
-    displayMembers(data.)
+async function getMembersData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayMembers(data.members);
 }
+
+const displayMembers = (members) => {
+  members.forEach((member) => {
+    const section = document.createElement("section");
+    const image = document.createElement("img");
+    const address = document.createElement("p");
+    const phone = document.createElement("p");
+    const anchor = document.createElement("a");
+    image.setAttribute("href", member.url);
+    image.setAttribute("alt",`${company.name} logo` );
+    address.textContent = member.address;
+  });
+};
