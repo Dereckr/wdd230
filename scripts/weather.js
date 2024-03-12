669a880333bd919c305862095764a2f67952aee5
Dereckr <dereckromos@gmail.com>
2024-03-11 22:26:49 -0400

    update

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,15 +1,16 @@
-const currentTemp = document.querySelector("#current-temp");
+const currentTemp = document.querySelector("#temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
+const description = document.querySelector("figcaption");
 
 const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=49.75006782093673&lon=6.635379449762701&units=imperial&appid=0ca2cf93a0895e3223f4ebe93fd2dd44";
+  "https://api.openweathermap.org/data/2.5/weather?lat=-16.550260968640423&lon=-68.09315877399598&units=metric&appid=0ca2cf93a0895e3223f4ebe93fd2dd44";
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
+      console.log(data);
       displayResults(data);
     } else {
       throw Error(await response.text());
@@ -20,13 +21,13 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   console.log(iconsrc);
   let desc = data.weather[0].description;
   weatherIcon.setAttribute("src", iconsrc);
   weatherIcon.setAttribute("alt", "weather icon");
-  captionDesc.textContent = `${desc.toUpperCase()}`;
+  description.textContent = `${desc.toUpperCase()}`;
 }
 
 apiFetch();
