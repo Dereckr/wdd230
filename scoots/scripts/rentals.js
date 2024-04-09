48e1826cafd60ae186f0f16c5d49793117a01529
Dereckr <dereckromos@gmail.com>
2024-04-09 17:07:30 -0400

    update

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -11,17 +11,31 @@ async function getPrices(urlR) {
 const displayPrices = (prices) => {
   prices.forEach((price) => {
     const tr = document.createElement("tr");
-    const th1 = document.createElement("th");
-    const th2 = document.createElement("th");
-    const th3 = document.createElement("th");
-    const th4 = document.createElement("th");
-    const th5 = document.createElement("th");
-    const th6 = document.createElement("th");
-    th1.textContent = price.type;
-    th2.textContent = price.MaxPersons;
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
 
-    tr.appendChild(th1);
-    tr.appendChild(th2);
+    td1.textContent = price.type;
+    td2.textContent = price.maxPersons;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    price.reservation.forEach((reserv) => {
+      const td3 = document.createElement("td");
+      const td4 = document.createElement("td");
+      td3.textContent = reserv.halfDay;
+      td4.textContent = reserv.fullDay;
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+    });
+    price.walkIn.forEach((w) => {
+      const td5 = document.createElement("td");
+      const td6 = document.createElement("td");
+      td5.textContent = w.halfDay;
+      td6.textContent = w.fullDay;
+      tr.appendChild(td5);
+      tr.appendChild(td6);
+    });
 
     rentals.appendChild(tr);
   });
